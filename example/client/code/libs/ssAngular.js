2ecaf972c8f71b1cff6b72fec3042c2304c0f414
Ben Polidore <polidore@gmail.com>
2012-07-02 09:08:11 -0400

    improved readme

--- example/client/code/libs/ssAngular.js
+++ example/client/code/libs/ssAngular.js
@@ -1,1 +1,17 @@
-../../../../lib/ssAngular.js
+angular.module('ssAngular',[])
+  .factory('pubsub', function() {
+    return function(scope) {
+      //override the $on function
+      var old$on = scope.$on;
+      scope.$on = function(name, listener) {
+        if(name.length > 3 && name.substr(0,3) === 'ss-') {
+          ss.event.on(name, function(message) {
+            scope.$broadcast(name, message);
+            scope.$digest();
+          });
+        }
+        //make sure to call angular's version
+        old$on.apply(this, [name, listener]);
+      };
+    };
+  });
