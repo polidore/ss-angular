9f93f039983df62b369c66ddffdd12389b7fbace
Ben Polidore <polidore@gmail.com>
2012-07-03 13:52:34 -0400

    refactor. prob breaking things\!

--- example/client/code/libs/pubsub.js
+++ example/client/code/libs/pubsub.js
@@ -1,18 +1,16 @@
-angular.module('ssAngular',[])
-  .factory('pubsub', function() {
-    return function(scope) {
-      //override the $on function
-      var old$on = scope.$on;
-      scope.$on = function(name, listener) {
-        if(name.length > 3 && name.substr(0,3) === 'ss-') {
-          ss.event.on(name, function(message) {
-            scope.$apply(function() {
-              scope.$broadcast(name, message);
-            });
+angular.module('ssAngular',['ng'])
+  .factory('pubsub', function($rootScope) {
+    //override the $on function
+    var old$on = $rootScope.prototype.$on;
+    $rootScope.protoype.$on = function(name, listener) {
+      if(name.length > 3 && name.substr(0,3) === 'ss-') {
+        ss.event.on(name, function(message) {
+          scope.$apply(function() {
+            scope.$broadcast(name, message);
           });
-        }
-        //make sure to call angular's version
-        old$on.apply(this, [name, listener]);
-      };
+        });
+      }
+      //make sure to call angular's version
+      old$on.apply(this, arguments);
     };
   });
