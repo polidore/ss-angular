14811d62e1b3a05172fc5d422137eaa39ac1b4fc
Ben Polidore <polidore@gmail.com>
2012-07-05 23:34:08 -0400

    fixed some bugs, but the example sucks. need to redo it a bit since you can't pass the intervalId from node over the wire easily.

--- example/client/code/app/controllers.js
+++ example/client/code/app/controllers.js
@@ -1,10 +1,21 @@
 angular.module('exampleApp', ['ssAngular'])
   .controller('SSCtrl',function($scope,pubsub,rpc) {
     $scope.messages = []
+    $scope.streaming = false;
+    $scope.intervalId = 0;
+
     $scope.$on('ss-example', function(event,msg) {
       $scope.messages.push(msg);
     });
+
     $scope.toggleData = function() {
-      $scope.streamiing = rpc('example.toggle');
+      if(!$scope.streaming) {
+        $scope.streaming = true;
+        $scope.intervalId = rpc('example.on');
+      }
+      else {
+        $scope.streaming = false;
+        rpc('example.off', $scope.intervalId);
+      }
     };
   });
--- example/server/rpc/example.js
+++ example/server/rpc/example.js
@@ -1,23 +1,19 @@
 exports.actions = function(req,res,ss) {
   var crypto = require('crypto');
-  var streaming = false;
-  var intervalId;
 
   return {
-    toggle: function() {
-      streaming = !streaming;
-      if(streaming) {
-        intervalId = setInterval(function() {
-          crypto.randomBytes(16, function(ex,buf) {
-            var message = 'Message from space: ' + buf;
-            ss.publish.all('ss-example', message);
-          });
-        }, 3000);
-      }
-      else {
-        clearInterval(intervalId);
-      }
-      return streaming;
+    on: function() {
+      var intervalId = setInterval(function() {
+        crypto.randomBytes(16, function(ex,buf) {
+          var message = 'Message from space: ' + buf;
+          ss.publish.all('ss-example', message);
+        });
+      }, 3000);
+      res(42); //intervalId is a complex object. great.
+    },
+    off: function(intervalId) {
+      console.log("Received intervalId: %s", intervalId);
+      clearInterval(intervalId);
     }
   };
 }
